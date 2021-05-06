3b8da452e8238b8de1ab1509de0b6d1e94f012e0
Ömer Ügür <79576871+oemerueguer@users.noreply.github.com>
2021-05-06 12:23:15 +0200

    updated Q2

--- join_operation/sqlzoo_join.sql
+++ join_operation/sqlzoo_join.sql
@@ -7,7 +7,24 @@ To identify German players, check for: teamid = 'GER'
 */
 /*      SOLUTION       */
 
+SELECT matchid, player FROM goal 
+WHERE teamid = 'GER';
 
+
+/*
+2. From the previous query you can see that Lars Bender's scored a goal in game 1012. 
+Now we want to know what teams were playing in that match.
+
+Notice in the that the column matchid in the goal table corresponds to the id column in the game table. 
+We can look up information about game 1012 by finding that row in the game table.
+
+Show id, stadium, team1, team2 for just game 1012
+*/
+
+/*      SOLUTION       */
+
+SELECT id,stadium,team1,team2 FROM game
+WHERE id = '1012';
 
 
 
